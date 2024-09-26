d15a1c327a4d7550a7e4b2b17a20b94f9d7d6189
cristiancat <cristian.catalan@stucom.com>
2024-09-26 19:31:21 +0200

    exemple audio i objectes predefinits

--- pr02/exemple.js
+++ pr02/exemple.js
@@ -12,7 +12,55 @@ let resultat= document.getElementById("resultat")
 resultat.innerText=numero;
 
 function sumaValors(){
-    resultat.innerText = input_numero.value;
+    // resultat.innerText = input_numero.value;
+    let num1 =  input_numero.value;
+    let num2 = document.getElementById("input_numero2").value;
+    if( isNaN(num1) ){
+        num1=0;
+    }
+    if(isNaN(num2)){
+        num2=0;
+    }
+    resultat.innerText= parseFloat(num1)+ parseFloat(num2);
+}
+
+let audio = document.getElementById("audio");
+
+function playAudio(){
+    audio.play();
+}
+function playAudio2(){
+    audio.src="FANFARE1.WAV";
+    audio.play();
 }
 
 
+// 1.	Crea un document HTML amb un div amb id “llista_propietats”. Programa amb 
+// JS que es creï una llista amb els següents missatges i amb els valors obtinguts dinàmicament:
+// a.	Valor mínim que pot tenir un número JS
+// b.	Amplada total de la pantalla
+// c.	Amplada interna de la finestra
+// d.	Títol de la web
+// e.	Hora actual
+
+//mostraLlista()
+window.setInterval(mostraLlista() , 1000)
+function mostraLlista(){
+    return ()=>{
+    let data = new Date();
+    let llista = document.getElementById("llista_propietats");
+    llista.innerHTML=`<ol> 
+                        <li>Valor mínim que pot tenir un número JS: `+Number.MIN_VALUE +` </li>
+                        <li> Amplada total de la pantalla: `+screen.width+` </li>
+                        <li>Amplada interna de la finestra:`+window.innerWidth+`</li>
+                        <li>Titol de la web: `+document.title+` </li>
+                        <li>Hora actual: `+data.getDate()
+                                        +`/`+data.getMonth()
+                                        +`/`+data.getFullYear()+
+                                        ` - `+ data.getHours()
+                                        +":"+data.getMinutes()
+                                        +":"+data.getSeconds()+
+                                        ` </li>
+                    </ol>`;
+    }
+}
